1b55944f29f17943f7dbce1da45fa5c859ca219e
WolverinDEV <git@did.science>
2026-07-09 14:15:14 +0200

    fix(signature): updated engines build info signature accordingly to the latest CS2 update

--- cs2/src/offsets.rs
+++ cs2/src/offsets.rs
@@ -69,7 +69,7 @@ impl CS2Offset {
                 Module::Engine,
                 Signature::relative_address(
                     obfstr!("client build info"),
-                    obfstr!("48 8B 1D ? ? ? ? 48 8D 3D"),
+                    pattern!("48 8B 05 ? ? ? ? 48 85 C0 48 0F 45 C8"),
                     0x03,
                     0x07,
                 ),
